c1163e91ed85bba46c2d4876641fa5c9284029bb
Андрей Ивойлов <a.ivoylov@bars-open.ru>
2019-03-28 05:22:02 +0300

    removed unused views, static content

--- SeaBattle.Server/Controllers/HomeController.cs
+++ SeaBattle.Server/Controllers/HomeController.cs
@@ -58,25 +58,6 @@
             return View(model);
         }
 
-        public IActionResult About() 
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
